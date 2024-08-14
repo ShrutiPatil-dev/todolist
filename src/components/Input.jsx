95c4ab657a185187a51b57e5fbae62a2a955881e
Shruti Patil <66897209+ShrutiPatil-dev@users.noreply.github.com>
2024-08-14 17:40:34 +0530

    Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -46,13 +46,13 @@ function Input() {
                     placeholder='Description of Todo Item...'
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    className='w-2/5 lg:w-3/4 m-5 p-2 lg:p-3 outline-none border-[1px] text-center text-gray-700 border-gray-100 bg-neutral-100 rounded-full placeholder:text-center'
+                    className='w-3/5 lg:w-3/4 m-5 p-2 lg:p-3 outline-none border-[1px] text-center text-gray-700 border-gray-100 bg-neutral-100 rounded-full placeholder:text-center'
                 />
                
                 
                 <button
                     type='submit'
-                    className='w-[100px] rounded-full bg-gray-800  m-5 p-2 lg:p-3 text-white text-base'
+                    className='w-[80px] lg:w-[100px] rounded-full bg-gray-800  m-4 p-2 lg:p-3 text-white text-sm lg:text-base'
                 >
                     Add Todo
                 </button>
